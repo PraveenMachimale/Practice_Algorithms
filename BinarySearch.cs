a079d4b74949a78a5aad6c7800b9b4fe31b105e6
PraveenMachimale <praveenmr2001@gmail.com>
2016-07-28 15:36:33 +0530

    Add files via upload

---
+++ BinarySearch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySerach
+{
+    class BinarySearch
+    {
+        public  bool BinarySerach(List<int> intArray, int serachNumber)
+        {
+
+            intArray.Sort();
+            int firstItem = intArray[0];
+            int lastItem = intArray[intArray.Count - 1];
+
+            while (firstItem <= lastItem)
+            {
+
+                int middleItem = intArray[(intArray.FindIndex(x => x == firstItem) + intArray.FindIndex(x => x == lastItem)) / 2];
+
+                if (serachNumber == middleItem)
+                    return true;
+                if (serachNumber < middleItem)
+                    lastItem = intArray[(intArray.FindIndex(x => x == firstItem) + intArray.FindIndex(x => x == lastItem)) / 2 - 1];
+                else
+                    firstItem = intArray[(intArray.FindIndex(x => x == firstItem) + intArray.FindIndex(x => x == lastItem)) / 2 + 1];
+            }
+
+            return false;
+
+        }
+    }
+}
